81af4deae2123d735e69f664e63628e091a88f61
kaycut <to_afendiko@hotmail.com>
2026-05-11 16:41:55 +0200

    Initial Commit

---
+++ DjGuard/DjGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DjGuardApp.swift
+//  DjGuard
+//
+//  Created by kay cut on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DjGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
